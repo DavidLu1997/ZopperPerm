d878b3d7b4edc389d39ff0158150b7795c7dd613
Zopper9000 <david.lu@hotmail.com>
2016-04-21 15:12:22 -0400

    improved memory efficency

--- Factorial.cs
+++ Factorial.cs
@@ -8,12 +8,8 @@ namespace ZopperPerm
 {
     public partial class CalculatePermutations 
     {
-        //Maximum factorial using ulong
-        //Going above 20 is pointless
-        private const int maxF = 20;
-
-        //Precomputed factorials, up to maxF
-        private List<ulong> facts = new List<ulong>() { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000 };
+        //Precomputed factorials, up to 20
+        private const List<ulong> facts = new List<ulong>() { 1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000, 20922789888000, 355687428096000, 6402373705728000, 121645100408832000, 2432902008176640000 };
 
         //Calculate factorial
         private ulong factorial(int n)
